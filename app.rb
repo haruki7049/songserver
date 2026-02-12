46299b8d4ef79dae4af27e8063061023c24ebfc5
haruki7049 <tontonkirikiri@gmail.com>
2026-02-13 05:54:20 +0900

    Update

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require "sinatra"
 
 get "/" do
-  rbfiles = File.join("**", "*.rb")
-  erbfiles = File.join("**", "*.erb")
-  @filenames = Dir.glob([rbfiles, erbfiles])
+  flac_files = File.join("resources/musics/**", "*.flac")
+  @flac_filenames = Dir.glob(flac_files)
+  mp3_files = File.join("resources/musics/**", "*.mp3")
+  @mp3_filenames = Dir.glob(mp3_files)
 
-  @title = "index"
-  @name = "Haruki"
   erb :index
+end
+
+get '/:filename' do |filename|
+  send_file "./#{filename}", :filename => filename, :type => 'Application/octet-stream'
 end
