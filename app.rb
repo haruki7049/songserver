036871086ad3e853b2fb541c0ca5bf578cab8723
haruki7049 <tontonkirikiri@gmail.com>
2026-02-13 05:48:34 +0900

    Update

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require "sinatra"
 
 get "/" do
+  rbfiles = File.join("**", "*.rb")
+  erbfiles = File.join("**", "*.erb")
+  @filenames = Dir.glob([rbfiles, erbfiles])
+
   @title = "index"
   @name = "Haruki"
   erb :index
