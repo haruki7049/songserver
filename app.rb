d31b4f05007fb0efd4a74d56e3336ed9a0831682
haruki7049 <tontonkirikiri@gmail.com>
2026-02-13 06:31:52 +0900

    Update

--- app.rb
+++ app.rb
@@ -1,12 +1,37 @@
 require "sinatra"
+require "zip"
+
+flac_paths = Dir.glob(File.join("resources/musics/**", "*.flac"))
+mp3_paths = Dir.glob(File.join("resources/musics/**", "*.mp3"))
+
+def create_zip(zip_name, entries)
+  Zip::File.open(zip_name, create: true) do |zip|
+    entries.each do |filename|
+      zip.add(filename.delete_prefix("resources/musics/"), filename)
+    end
+  end
+end
 
 get "/" do
-  flac_files = File.join("resources/musics/**", "*.flac")
-  @flac_filenames = Dir.glob(flac_files)
-  mp3_files = File.join("resources/musics/**", "*.mp3")
-  @mp3_filenames = Dir.glob(mp3_files)
+  flac_list = flac_paths.map do |path|
+    path.delete_prefix "resources/musics"
+  end
+  mp3_list = mp3_paths.map do |path|
+    path.delete_prefix "resources/musics"
+  end
+
+  @flac_list = flac_list
+  @mp3_list = mp3_list
 
   erb :index
+end
+
+get "/flac" do
+  date = DateTime.now
+  zip_name = "./resources/zips/flac-files-#{date}.zip"
+  create_zip zip_name, flac_paths
+
+  send_file zip_name
 end
 
 get '/:filename' do |filename|
--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@
 pkgs.mkShell {
   nativeBuildInputs = [
     # Runtime
-    (pkgs.ruby.withPackages (ps: [ ps.sinatra ps.rackup ps.puma ]))
+    (pkgs.ruby.withPackages (ps: [ ps.sinatra ps.rackup ps.puma ps.rubyzip ]))
 
     # LSP
     pkgs.nil # Nix
