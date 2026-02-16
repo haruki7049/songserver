dd5bb840917fc48d61b9a5afc401cd3bf67adebd
haruki7049 <tontonkirikiri@gmail.com>
2026-02-16 10:25:46 +0900

    style: nix fmt

--- app/src/main/java/dev/haruki7049/songserver/Runner.java
+++ app/src/main/java/dev/haruki7049/songserver/Runner.java
@@ -2,15 +2,10 @@ package dev.haruki7049.songserver;
 
 import java.util.logging.Logger;
 
-/**
- * Handles the initialization and execution of the songserver.
- */
+/** Handles the initialization and execution of the songserver. */
 public class Runner {
   private static final Logger logger = Logger.getLogger("songserver");
 
-  /**
-   * Runs the main songserver logic.
-   */
-  public static void run() throws Exception {
-  }
+  /** Runs the main songserver logic. */
+  public static void run() throws Exception {}
 }
