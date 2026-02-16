f6444a2c6ae957020ddccc936714df57dcc3fe16
haruki7049 <tontonkirikiri@gmail.com>
2026-02-16 11:20:31 +0900

    Update Cli.java

--- app/src/main/java/dev/haruki7049/songserver/Cli.java
+++ app/src/main/java/dev/haruki7049/songserver/Cli.java
@@ -7,22 +7,8 @@ import picocli.CommandLine;
  * CLI (Command Line Interface) handler class for songserver. This class uses picocli to parse
  * command line arguments.
  */
-@CommandLine.Command(name = "songserver")
+@CommandLine.Command(name = "songserver", mixinStandardHelpOptions = true)
 public class Cli implements Callable<Integer> {
-  /** Option to display the help message. */
-  @CommandLine.Option(
-      names = {"-h", "--help"},
-      description = "show this command's help",
-      usageHelp = true)
-  boolean showHelp;
-
-  /** Option to display the version information. */
-  @CommandLine.Option(
-      names = {"-v", "--version"},
-      description = "show this command's version",
-      versionHelp = true)
-  boolean showVersion;
-
   @Override
   public Integer call() throws Exception {
     Runner.run();
