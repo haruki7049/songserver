519d2c0e363b9ad1dc42f316ebf6c62c4b6d0b78
haruki7049 <tontonkirikiri@gmail.com>
2026-02-17 16:43:17 +0900

    Update

---
+++ app/src/main/java/dev/haruki7049/songserver/Config.java
@@ -0,0 +1,9 @@
+package dev.haruki7049.songserver;
+
+import org.springframework.context.annotation.Configuration;
+
+/** A Configuration class. */
+@Configuration
+public class Config {
+  
+}
--- app/src/main/java/dev/haruki7049/songserver/Zipper.java
+++ app/src/main/java/dev/haruki7049/songserver/Zipper.java
@@ -1,8 +1,15 @@
 package dev.haruki7049.songserver;
 
+import java.io.File;
 import org.springframework.stereotype.Service;
 
 /** A class to create a Zip file. */
 @Service
 public class Zipper {
+
+  /** A function to create Zip file. */
+  public File createZip() {
+    File result = new File("test.txt");
+    return result;
+  }
 }
